76f2682199a3d6274cccc8164c540939e81f4571
dewaleolaoye <esix@localhost.localdomain>
2019-03-11 15:20:01 +0100

    Arguments Parsing with Yargs I

--- note-app/app.js
+++ note-app/app.js
@@ -1,9 +1,38 @@
 const validator = require("validator");
 const chalk = require("chalk");
-const getNotes = require("./notes");
+const yargs = require("yargs");
+const command = process.argv[2];
 
-const msg = getNotes();
-console.log(msg);
-
-console.log(chalk.red.bold(validator.isURL("https://goal.com")));
-console.log(chalk.green("Success"));
+// create the add command
+yargs.command({
+  command: "add",
+  describe: "use to add new note",
+  hancler: function() {
+    console.log("Add a new note");
+  }
+});
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  hancler: function() {
+    console.log("remove note");
+  }
+});
+// create list command
+yargs.command({
+  command: "list",
+  describe: "list a note",
+  hancler: function() {
+    console.log("list note");
+  }
+});
+// read
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  hancler: function() {
+    console.log("read note");
+  }
+});
+console.log(yargs.argv);
