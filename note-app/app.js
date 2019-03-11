76a883ca99057feb9790342fa1949994da7e285e
dewaleolaoye <esix@localhost.localdomain>
2019-03-11 12:40:39 +0100

    Module System(Note App)

--- note-app/app.js
+++ note-app/app.js
@@ -1,4 +1,9 @@
+const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes");
 
 const msg = getNotes();
 console.log(msg);
+
+console.log(chalk.red.bold(validator.isURL("https://goal.com")));
+console.log(chalk.green("Success"));
