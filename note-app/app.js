753c926d071129b638ae6bf4237287d1b6aa4641
dewaleolaoye <esix@localhost.localdomain>
2019-03-11 12:06:37 +0100

    import files

---
+++ note-app/app.js
@@ -0,0 +1,5 @@
+// const add = require("./utils");
+// // const name = "Adewale";
+
+// const sum = add(5, -2);
+// console.log(sum);
---
+++ note-app/utils.js
@@ -0,0 +1,11 @@
+// console.log("utils.js");
+
+// const name = "Adewale Olaoye";
+
+// module.exports = name;
+
+const add = function(a, b) {
+  return a + b;
+};
+
+module.exports = add;
