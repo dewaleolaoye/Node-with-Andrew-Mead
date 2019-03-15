a724bd84992c4c3871d885d10fb8f48cff4b265a
dewaleolaoye <esix@localhost.localdomain>
2019-03-15 17:36:16 +0100

    making http request

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -3,7 +3,12 @@ const request = require("request");
 const url =
   "https://api.darksky.net/forecast/60bb25a3134e0ba16fcdff69d7666fcf/37.8267,-122.4233";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+    "It is currently " +
+      response.body.currently.temperature +
+      " degrees out and " +
+      response.body.currently.precipProbability +
+      "% of rain"
+  );
 });
