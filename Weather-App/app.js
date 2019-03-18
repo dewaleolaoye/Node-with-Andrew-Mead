fd817abb63c2611ce6ab6b74099faeaa21822648
dewaleolaoye <esix@localhost.localdomain>
2019-03-18 17:39:37 +0100

    callback abstraction

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,5 +1,6 @@
 const request = require("request");
 const chalk = require("chalk");
+const geocode = require("./utils/geocode");
 
 // const url =
 //   "https://api.darksky.net/forecast/60bb25a3134e0ba16fcdff69d7666fcf/37.8267,-122.4233";
@@ -25,20 +26,25 @@ const chalk = require("chalk");
 
 // Geocoding
 
-const geocodeURL =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGV3YWxlb2xhb3llIiwiYSI6ImNqdGUyMDRuejFhZmMzeXA4bTB2NnB4dWwifQ.1IADYxsVwk94LDaeYoizzg&limit=1";
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.log(chalk.red.inverse("Unable to connect"));
-  } else if (response.body.features.length === 0) {
-    console.log(
-      chalk.red.inverse("Unable to find loation, Try another search")
-    );
-  } else {
-    const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(
-      chalk.green.inverse("latitude: " + latitude, "longitude: " + longitude)
-    );
-  }
+// const geocodeURL =
+//   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGV3YWxlb2xhb3llIiwiYSI6ImNqdGUyMDRuejFhZmMzeXA4bTB2NnB4dWwifQ.1IADYxsVwk94LDaeYoizzg&limit=1";
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//   if (error) {
+//     console.log(chalk.red.inverse("Unable to connect"));
+//   } else if (response.body.features.length === 0) {
+//     console.log(
+//       chalk.red.inverse("Unable to find loation, Try another search")
+//     );
+//   } else {
+//     const latitude = response.body.features[0].center[1];
+//     const longitude = response.body.features[0].center[0];
+//     console.log(
+//       chalk.green.inverse("latitude: " + latitude, "longitude: " + longitude)
+//     );
+//   }
+// });
+
+geocode("Keffi", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
---
+++ Weather-App/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+const chalk = require("chalk");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoiZGV3YWxlb2xhb3llIiwiYSI6ImNqdGUyMDRuejFhZmMzeXA4bTB2NnB4dWwifQ.1IADYxsVwk94LDaeYoizzg&limit=1";
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to Location", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location, Try another search", undefined);
+    } else {
+      callback(undefined, {
+        longitude: response.body.features[0].center[1],
+        latitude: response.body.features[0].center[1],
+        location: response.body.features[0].place_name
+      });
+    }
+  });
+};
+
+module.exports = geocode;
