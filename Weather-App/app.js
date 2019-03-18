9ae1d52d698673130b5ce43e1bcd935fb1a681ab
dewaleolaoye <esix@localhost.localdomain>
2019-03-18 08:49:54 +0100

    with units

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,11 +1,12 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/60bb25a3134e0ba16fcdff69d7666fcf/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/60bb25a3134e0ba16fcdff69d7666fcf/37.8267,-122.4233?units=si";
 
 request({ url: url, json: true }, (error, response) => {
   console.log(
-    "It is currently " +
+    response.body.hourly.data[0].summary +
+      " It is currently " +
       response.body.currently.temperature +
       " degrees out and " +
       response.body.currently.precipProbability +
