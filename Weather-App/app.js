2308775948a130ac0c66af89e97ca51007bb624d
dewaleolaoye <esix@localhost.localdomain>
2019-03-18 09:39:15 +0100

    HTTP request Challenge

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/60bb25a3134e0ba16fcdff69d7666fcf/37.8267,-122.4233?units=si";
+  "https://api.darksky.net/forecast/60bb25a3134e0ba16fcdff69d7666fcf/37.8267,-122.4233";
 
 request({ url: url, json: true }, (error, response) => {
   console.log(
@@ -12,4 +12,14 @@ request({ url: url, json: true }, (error, response) => {
       response.body.currently.precipProbability +
       "% of rain"
   );
+});
+
+const geocodeURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGV3YWxlb2xhb3llIiwiYSI6ImNqdGUyMDRuejFhZmMzeXA4bTB2NnB4dWwifQ.1IADYxsVwk94LDaeYoizzg&limit=1";
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+
+  console.log("latitude: " + latitude);
+  console.log("longitude: " + longitude);
 });
