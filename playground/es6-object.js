1a5ae48c3aecf4d420aa36ca60b7ad2fad9c34b0
dewaleolaoye <adewaleolaoye@gmail.com>
2019-03-19 09:58:47 +0100

    loop through objects

--- playground/es6-object.js
+++ playground/es6-object.js
@@ -18,7 +18,28 @@
 // let [firstName, , title] = "John Doe Iskolo".split(" ");
 // console.log(firstName, title);
 
-let johnDoe = {};
+// let johnDoe = {};
 
-[johnDoe.first, johnDoe.last, johnDoe.title] = "John Doe Iskolo".split(" ");
-console.log(johnDoe.first, johnDoe.title);
+// [johnDoe.first, johnDoe.last, johnDoe.title] = "John Doe Iskolo".split(" ");
+// console.log(johnDoe.first, johnDoe.title);
+
+let obj = {
+  firstName: "John",
+  lastName: "Doe",
+  title: "Iskolo"
+};
+
+Object.keys(obj).forEach(key => {
+  console.log(`${key}: ${obj[key]}`);
+});
+
+console.log("===A New line===");
+let obj2 = {
+  firstName: "John",
+  lastName: "Doe",
+  title: "Iskolo"
+};
+
+for (let [key, value] of Object.entries(obj2)) {
+  console.log(`${key}: ${value}`);
+}
