29ee682c9e0958e83a8b4d2995529fc8b9677b1c
dewaleolaoye <adewaleolaoye@gmail.com>
2019-03-19 09:33:17 +0100

    working with destructuring

---
+++ playground/es6-object.js
@@ -0,0 +1,16 @@
+// const product = {
+//   label: "gold",
+//   price: 2,
+//   stock: 5,
+//   salesPrice: undefined
+// };
+
+// const { label, stock } = product;
+
+// console.log(label);
+// console.log(stock);
+
+let johnDoe = ["John", "Doe", "Iskolo"];
+
+let [firstName, lastName, title] = johnDoe;
+console.log(firstName, lastName, title);
