8dcc5a805a63ed8675f70e7c455fb702e48495c8
dewaleolaoye <adewaleolaoye@gmail.com>
2019-03-19 09:45:37 +0100

    resources

--- playground/es6-object.js
+++ playground/es6-object.js
@@ -10,7 +10,15 @@
 // console.log(label);
 // console.log(stock);
 
-let johnDoe = ["John", "Doe", "Iskolo"];
+// let johnDoe = ["John", "Doe", "Iskolo"];
 
-let [firstName, lastName, title] = johnDoe;
-console.log(firstName, lastName, title);
+// let [firstName, lastName, title] = johnDoe;
+// console.log(firstName, lastName, title);
+
+// let [firstName, , title] = "John Doe Iskolo".split(" ");
+// console.log(firstName, title);
+
+let johnDoe = {};
+
+[johnDoe.first, johnDoe.last, johnDoe.title] = "John Doe Iskolo".split(" ");
+console.log(johnDoe.first, johnDoe.title);
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 ## Learning Node JS with Andrew Mead
+
+# scotch.io materials on Destructuring
+
+# https://scotch.io/tutorials/destructuring-assignment-in-javascript by Chris Nwamba
