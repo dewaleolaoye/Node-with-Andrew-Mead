d3fd3b0003f147080c571f5c3a202b6dc7077fa0
dewaleolaoye <adewaleolaoye@gmail.com>
2019-03-20 18:42:34 +0100

    Customizing the views Directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// Define path for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
+// Setup for handlebars engine and root location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+// Setup Static Directory to serve
 app.use(express.static(publicDirectoryPath));
 
 // root page
