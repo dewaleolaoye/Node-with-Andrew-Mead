978d91185cb4035708f90e14c4c6888e1de15a50
dewaleolaoye <adewaleolaoye@gmail.com>
2019-03-20 17:40:35 +0100

    Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,9 +4,33 @@ const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+// root page
 app.get("", (req, res) => {
-  res.send("<h1>Hello Express</h1>");
+  res.render("index", {
+    title: "Weather",
+    name: "Adewale Olaoye"
+  });
+});
+
+// about page
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Adewale Olaoye"
+  });
+});
+
+// help page
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    email: "adewaleolaoye21@gmail.com",
+    address: "22, Association Close Ogudu"
+  });
 });
 
 app.get("/weather", (req, res) => {
