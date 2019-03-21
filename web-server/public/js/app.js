f2751e1c8b739250e5d01c29cc427c8530a0a0b0
dewaleolaoye <adewaleolaoye@gmail.com>
2019-03-21 16:54:22 +0100

    from browser

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
-console.log('Client side javascript file is loaded!')
+console.log("Client side javascript file is loaded and it's working!");
+
+fetch("http://localhost:3000/weather?address=lafia").then(response => {
+  response.json().then(data => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -48,23 +48,26 @@ app.get("/weather", (req, res) => {
     });
   }
 
-  geocode(req.query.address, (error, { latitude, longitude, location }) => {
-    if (error) {
-      return res.send({ error });
-    }
-
-    forecast(latitude, longitude, (error, forecastData) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({ error });
       }
 
-      res.send({
-        forecast: forecastData,
-        location,
-        address: req.query.address
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 app.get("/products", (req, res) => {
