b661ca802241b8a0c51569940a021974afae872b
Engr - six <esix@localhost.localdomain>
2019-03-26 09:02:00 +0100

    setup app for heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -100,6 +100,7 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
